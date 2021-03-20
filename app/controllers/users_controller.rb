8bd609f522af5f8d0228195c807b478cbdde96bb
Sergio D Perdomo <sergio.931129@gmail.com>
2021-03-19 23:57:55 -0500

    Modify controller to accept json, modify user view to show photo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,19 @@ class UsersController < ApplicationController
   # GET /users or /users.json
   def index
     @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @users }
+    end
   end
 
   # GET /users/1 or /users/1.json
   def show
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render :json => @user }
+    end
   end
 
   # GET /users/new
